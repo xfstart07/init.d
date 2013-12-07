50fcf29413c4c0d31dae0e77d9ca7e82efd0b14d
Xu Fei <xfstart07@gmail.com>
2013-12-07 13:28:30 +0800

    add rvm install

--- install_rvm.sh
+++ install_rvm.sh
@@ -1,14 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Install RVM"
-echo "...............begin.................."
-bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
-echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"' >>~/.bash_profile
-source ~/.bash_profile
-echo ""
-echo"source ~/.bashrc"
-echo "rvm pkg install readline"
-echo "rvm install 1.9.3"
-echo "rvm use 1.9.3 --default"
-echo ""
-echo "...............end....................."
+
+\curl -sSL https://get.rvm.io | bash -s stable --ignore-dotfiles
+
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"' >>~/.bashrc
+source ~/.bashrc
+
+source ~/.rvm/scripts/rvm
+
+rvm install 2.0.0
+rvm use 2.0.0 --default
