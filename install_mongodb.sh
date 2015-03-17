76591793f5d937f86855ebd9f04fcb5d0f0edb54
xfstart07 <xfstart07@gmail.com>
2015-03-17 23:53:31 +0800

    Update mongodb

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
-sudo apt-get update
-sudo apt-get install mongodb-10gen
+sudo apt-get install mongodb
 
 # set dbpath and mongodb.log
 echo "set dppath"
 
 sudo mkdir /data/
-sudo mkdir /data/mongodb/
-sudo mongod --dbpath /data/mongodb/
-sudo mongod --fork --logpath /var/log/mongodb.log
+sudo mkdir /data/db/
+#sudo mongod --dbpath /data/mongodb/
+#sudo mongod --fork --logpath /var/log/mongodb.log
 
 echo "restart mongodb"
 sudo service mongodb restart
