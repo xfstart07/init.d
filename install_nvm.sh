2cc0f7a64f25d867233d7353f1b90416a613bba0
Leon Xu <xfstart07@gmail.com>
2016-02-29 13:09:06 +0000

    fix nvm

--- install_nvm.sh
+++ install_nvm.sh
@@ -1,1 +1,7 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+VERSION="v0.31.0"
+NODE='5.0'
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/$VERSION/install.sh | bash
+
+source ~/.bashrc
+nvm install $NODE && nvm use $NODE 
